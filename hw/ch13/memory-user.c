505e257cbba9f101a79617a191a7f1e5080a6f48
Joel Savitz <joelsavitz@gmail.com>
2019-05-29 11:49:25 -0400

    add ch13 hw

---
+++ hw/ch13/memory-user.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define REEE_LENGTH 	100
+#define ARBITRARY_BYTE 	0xaa
+#define B_PER_MB 	1024 * 1024
+
+void usage(void) {
+	printf("Requires exactly one integer argument specifying the number of bytes to allocate\n") ;
+}
+
+int main(int argc, char * argv[]) {
+	size_t bytes, megabytes, counter ;
+	char * arr ;
+
+	if (argc != 2) {
+		usage() ;
+		exit(1) ;
+	}
+
+	megabytes = atoi(argv[1]) ;
+
+	bytes = megabytes * B_PER_MB ;
+
+	arr = malloc(bytes) ;
+	if (!arr) {
+		printf("memory error ahhhhh can't malloc! Quittttinngggggg\n") ;
+		putchar('r') ;
+		for (counter = 0; counter < REEE_LENGTH; ++counter) {
+			putchar('e') ;
+		}
+		exit(2) ;
+	}
+	else {
+		printf("Alloc %zd megabytes OK.\n", megabytes) ;
+	}
+
+	/* Just do a bunch of useless calculations */
+	for (counter = 0 ; 1 ; counter = (counter + 1) % bytes) {
+		arr[counter] ^= ARBITRARY_BYTE ;
+	}
+
+	return 0 ;
+}
